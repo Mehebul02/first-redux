6b26eea672aca3843122d1bc6cc027cb025fba82
Mehebul02 <mehebul2123@gmail.com>
2025-01-01 01:34:10 +0600

    added the dispatch

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
-
 import './App.css'
+import { decrement, increment } from './redux/features/counter/counterSlice'
+import { useAppDispatch, useAppSelector } from './redux/hook'
 
 function App() {
+
+
+  const dispatch = useAppDispatch()
+
+  const { count } = useAppSelector((state) => state.counter)
+
+
+  const handleIncrement = () => {
+    dispatch(increment())
+  }
+  const handleDecrement = () => {
+    dispatch(decrement())
+  }
 
   return (
     <>
       <div>
         <h1>Counter with redux</h1>
 
-        <button className='bg-gray-950'>Increment</button>
-        <h1>0</h1>
-        <button>decrement</button>
+        <button onClick={handleIncrement} className='bg-gray-950'>Increment</button>
+        <h1>{count}</h1>
+        <button onClick={handleDecrement}>decrement</button>
       </div>
     </>
   )
