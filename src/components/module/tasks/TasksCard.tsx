d0ecb0eabc431b8f2507b4c71492185f7b3b464f
Mehebul02 <mehebul2123@gmail.com>
2025-01-11 22:59:25 +0600

    Successfully true and false

--- src/components/module/tasks/TasksCard.tsx
+++ src/components/module/tasks/TasksCard.tsx
@@ -1,15 +1,18 @@
 import { Button } from '@/components/ui/button';
 import { Checkbox } from '@/components/ui/checkbox';
 import { cn } from '@/lib/utils';
+import { toggleCompleteState } from '@/redux/features/task/taskSlice';
+import { useAppDispatch } from '@/redux/hook';
 import { ITask } from '@/types';
 import { Trash2 } from 'lucide-react';
-import React from 'react';
 
 interface IProps {
     task: ITask
 }
 
 const TasksCard = ({ task }: IProps) => {
+
+    const dispatch = useAppDispatch()
     return (
         <div className="border px-5 py-3 rounded-md ">
             <div className="flex justify-between items-center">
@@ -31,7 +34,7 @@ const TasksCard = ({ task }: IProps) => {
                     <Button variant="link" className="p-0 text-red-500">
                         <Trash2 />
                     </Button>
-                    <Checkbox />
+                    <Checkbox onClick={()=>dispatch(toggleCompleteState(task.id))} />
                 </div>
             </div>
             <p className='mt-5'>{task.description}</p>
--- src/redux/features/task/taskSlice.tsx
+++ src/redux/features/task/taskSlice.tsx
@@ -1,6 +1,6 @@
 import { RootState } from "@/redux/store";
 import { ITask } from "@/types";
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, nanoid, PayloadAction } from "@reduxjs/toolkit";
 import { v4 as uuidv4 } from 'uuid';
 
 interface InitialState {
@@ -62,21 +62,25 @@ const initialState: InitialState = {
     filter: "High"
 }
 
+type DraftTask = Pick<ITask, 'title' | 'description' | 'duaDate' | 'priority'>
+
+const createTask = (taskData: DraftTask): ITask => {
+    return { id: nanoid(), isCompleted: false, ...taskData }
+}
+
 const taskSlice = createSlice({
     name: 'task',
     initialState,
     reducers: {
-        addTask: (state, action: PayloadAction<ITask>) => {
-
-            const id = uuidv4()
-            const taskData = {
-                ...action.payload,
-                id,
-                isCompleted: false,
-
-
-            }
+        addTask: (state, action: PayloadAction<DraftTask>) => {
+            const taskData = createTask(action.payload)
             state.tasks.push(taskData)
+        },
+        toggleCompleteState: (state, action: PayloadAction<string>) => {
+            console.log(action)
+
+            state.tasks.forEach((task) => task.id === action.payload ? (task.isCompleted = !task.isCompleted) : task)
+
         }
     }
 })
@@ -89,5 +93,5 @@ export const selectFilter = (state: RootState) => {
     return state.todo.filter
 }
 
-export const { addTask } = taskSlice.actions;
+export const { addTask,toggleCompleteState } = taskSlice.actions;
 export default taskSlice.reducer
